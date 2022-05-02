29bde7defb372f13d347a932e5a766b1e4e0b2bf
Dhanalakshmi <98074723+tadisetti@users.noreply.github.com>
2022-05-02 18:40:36 +0530

    Add files via upload

---
+++ ArithematicException.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class ArrayIndexException
+  {
+    public static void main(String args[])
+    {
+      int i;
+      int a[]=new int[5];
+      Scanner sc=new Scanner(System.in);
+      {
+        System.out.println("Enter elements");
+        
+      }
+      for(i=0;i<=5;i++)
+        {
+          a[i]=sc.nextInt();
+          {
+            System.out.println(a[5]);
+          }
+        }
+    }
+  }
---
+++ ArrayIndexException.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class NumberformatException
+  {
+    public static void main(String args[])
+    {
+      String str="123.6";
+        int x=Integer.parseInt(str);
+      System.out.println(x);
+    }
+  }
